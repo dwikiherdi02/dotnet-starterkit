f1d050059eb550d54cc21260642688d0c1ec91bb
Dwiki Herdiansyah <dwikiherdi520@gmail.com>
2024-10-16 12:29:33 +0700

    chore: add .gitignore

--- Apps/obj/Debug/net8.0/Apps.AssemblyInfo.cs
+++ Apps/obj/Debug/net8.0/Apps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a44c88a01840b86b2a7ac76682c826d53a490ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c75d1451ba83b8aac57db05bd27cdb23d89421")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
