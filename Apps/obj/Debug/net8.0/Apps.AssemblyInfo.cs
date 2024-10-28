670bde57c5f3bdc2e704c12ef0cdd4767dccac8e
Dwiki Herdiansyah <dwikiherdi520@gmail.com>
2024-10-28 09:31:34 +0700

    chore: update object

--- Apps/obj/Debug/net8.0/Apps.AssemblyInfo.cs
+++ Apps/obj/Debug/net8.0/Apps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d74cda2c13cb0ebd4d7e7e4da0e222c87524a4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ec1654f230706b6040c4a7b3e3519f88bff470")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
